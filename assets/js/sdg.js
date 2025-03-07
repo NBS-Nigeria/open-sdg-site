47af2b437dfec0739bc2e2260fe1fcd0fb2beb6b
luckychenko <luckychenko@gmail.com>
2025-03-07 05:45:11 +0000

    Deploying to gh-pages from  @ 6c30bd714baed165514b5881869a28fd9bbccc04 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3282,7 +3282,7 @@ var mapView = function () {
     $('#map').sdgMap({
       indicatorId: indicatorId,
       mapOptions: {"disaggregation_controls":false,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":""}},
-      mapLayers: [{"subfolder":"country","label":"Nigeria","min_zoom":5,"max_zoom":10,"staticBorders":false},{"subfolder":"regions","label":"Zones","min_zoom":5,"max_zoom":10,"staticBorders":false},{"subfolder":"state","label":"States","min_zoom":5,"max_zoom":10,"staticBorders":false}],
+      mapLayers: [{"subfolder":"country","label":"Nigeria","min_zoom":5,"max_zoom":10,"staticBorders":false},{"subfolder":"regions","label":"Zones","min_zoom":5,"max_zoom":10,"staticBorders":false},{"subfolder":"states","label":"States","min_zoom":5,"max_zoom":10,"staticBorders":false}],
       precision: precision,
       precisionItems: precisionItems,
       decimalSeparator: decimalSeparator,
